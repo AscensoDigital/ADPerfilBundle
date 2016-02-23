08c757f7cbf0f24c69c8078ccef5195e83337e51
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-02-23 02:57:51 -0300

    se implementa js para realizar filtro asincronico

---
+++ Resources/public/js/ad_perfil.filtros.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    var frm_filtro=$('#ad_perfil-frm-filtros');
+    frm_filtro.on('submit',function(){
+        var error='';
+        frm_filtro.find(':input[required="required"]').each(function() {
+            var elemento= this;
+            ids=elemento.id.split('_');
+            ids.splice(0,1);
+            if($(elemento).val()===null || $(elemento).val().length===0) {
+                error+='Filtro ' + ids.join(' ') + " es obligatorio\n";
+            }
+        });
+        if(frm_filtro.data('auto-llenado')===true) {
+            frm_filtro.find(':input').each(function () {
+                var elemento = this;
+                ids = elemento.id.split('_');
+                ids.splice(0, 1);
+                var name = elemento.id;
+                if (elemento.multiple) {
+                    name = name + '_multiple'
+                }
+                if ($(elemento).val() === null || $(elemento).val().length === 0) {
+                    sessionStorage.removeItem(name);
+                }
+                else if (elemento.id != 'filtros__token') {
+                    sessionStorage.setItem(name, JSON.stringify($(elemento).val()));
+                }
+            });
+        }
+        if(''!=error) {
+            alert(error);
+            return false;
+        }
+
+        var destino="#" + frm_filtro.data('update');
+        var url= frm_filtro.attr('action');
+        $('#ad_perfil-btn-filtros').prepend('<i class="fa fa-refresh fa-spin"></i>').prop('disabled', true);
+        $.ajax({
+            type: 'POST',
+            url: url,
+            dataType : 'html',
+            data: frm_filtro.serialize(),
+            success: function(data) {
+                $(destino).html(data);
+                $('i.fa-spin').remove();
+                $('#ad_perfil-btn-filtros').prop('disabled', false);
+                $('#ad_perfil-collapseFiltro').collapse('hide');
+                return true;
+            },
+            error : function (obj) {
+                $('i.fa-spin').remove();
+                $('#ad_perfil-btn-filtros').prop('disabled', false);
+                alert("Error de Conexion: "+ obj.statusText);
+              return false;
+            }
+        });
+        return false;
+    });
+
+    $('#reset').on('click', function(){
+        frm_filtro.find(':input').each(function() {
+            $(this).val('');
+        });
+    });
+
+    var filtrar=false;
+    var requerido=true;
+    frm_filtro.find(':input').each(function() {
+        var elemento= this;
+        var name=elemento.id;
+        if(elemento.multiple){
+            name=name + '_multiple'
+        }
+        if(frm_filtro.data('auto-llenado')===true) {
+            if ($(elemento).val() === null || $(elemento).val().length === 0) {
+                var valor = JSON.parse(sessionStorage.getItem(name));
+                if (valor != null) {
+                    $(elemento).val(valor);
+                    if ($(elemento).val()) {
+                        $(elemento).change();
+                    }
+                }
+            }
+        }
+        if($(elemento).attr('required')=='required' && ($(elemento).val()===null || $(elemento).val().length===0)) {
+            requerido=false;
+        }
+        if($(elemento).val()) {
+            filtrar=true;
+        }
+    });
+
+    if(frm_filtro.data('auto-filter') === true && filtrar && requerido) {
+        frm_filtro.submit();
+    }
+    $('.fecha').datepicker({
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        dateFormat: "yy-mm-dd"
+    });
+});
