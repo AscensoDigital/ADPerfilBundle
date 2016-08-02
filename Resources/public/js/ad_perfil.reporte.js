d0ef67af8b6434d7a5dd55a71dd4efa09375409f
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-08-02 19:38:03 -0400

    se actualiza js de reportes

--- Resources/public/js/ad_perfil.reporte.js
+++ Resources/public/js/ad_perfil.reporte.js
@@ -5,9 +5,10 @@ $(document).ready(function () {
     $('a[data-toggle="modal"]').on('click', function(){
         var modal_id=$(this).attr('href');
         var url=$(this).data('url');
+        var nombre=$(this).data('reporte');
+        $('#rep-nombre').text(nombre);
         $(modal_id).find('a').each(function(){
             $(this).attr('href',url + '/' + $(this).data('valor'));
         });
-        $('#rep-nombre').text($(this).data('reporte'));
     });
 });
