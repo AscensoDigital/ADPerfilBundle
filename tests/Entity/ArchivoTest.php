7553441c772865dd9ba7a055c5c9000409d74a38
Claudio Corvalan <chiko6669@gmail.com>
2025-06-30 23:28:31 -0400

    aumento cobertura test ArchivoTest.php

--- tests/Entity/ArchivoTest.php
+++ tests/Entity/ArchivoTest.php
@@ -60,7 +60,7 @@ class ArchivoTest extends TestCase
     public function testGetNombre()
     {
         $archivo = new Archivo();
-        $archivo->setRuta('uploads/archivo123.pdf');
+        $archivo->setRuta('archivo123.pdf');
         $this->assertEquals('archivo123.pdf', $archivo->getNombre());
     }
 
@@ -72,5 +72,105 @@ class ArchivoTest extends TestCase
         $this->assertEquals('uploads', $archivo->getUploadDir());
         $this->assertStringEndsWith('/web/uploads/archivo.pdf', $archivo->getPath());
         $this->assertStringEndsWith('/web/uploads', $archivo->getUploadRootDir());
+    }
+
+    public function testSetAndGetFile()
+    {
+        $archivo = new Archivo();
+        $mockFile = $this->createMock(UploadedFile::class);
+        $archivo->setFile($mockFile);
+        $this->assertSame($mockFile, $archivo->getFile());
+    }
+
+    public function testSetAndGetCreador()
+    {
+        $archivo = new Archivo();
+        $creador = $this->createMock(UserInterface::class);
+        $archivo->setCreador($creador);
+        $this->assertSame($creador, $archivo->getCreador());
+    }
+
+    public function testSetAndGetFechaPublicacion()
+    {
+        $archivo = new Archivo();
+        $fecha = new \DateTime('2020-01-01');
+        $archivo->setFechaPublicacion($fecha);
+        $this->assertSame($fecha, $archivo->getFechaPublicacion());
+    }
+
+    public function testSetAndGetMimeType()
+    {
+        $archivo = new Archivo();
+        $archivo->setMimeType('application/pdf');
+        $this->assertEquals('application/pdf', $archivo->getMimeType());
+    }
+
+    public function testSetAndGetVisible()
+    {
+        $archivo = new Archivo();
+        $archivo->setVisible(true);
+        $this->assertTrue($archivo->getVisible());
+    }
+
+    public function testGetExtensionOriginalReturnsEmptyWhenFileIsNull()
+    {
+        $archivo = new Archivo();
+        $archivo->setFile(null);
+        $this->assertSame('', $archivo->getExtensionOriginal());
+    }
+
+    public function testSaveFileStoresCorrectly()
+    {
+        $archivo = new Archivo();
+
+        $fsMock = $this->getMockBuilder(Filesystem::class)
+            ->onlyMethods(['dumpFile'])
+            ->getMock();
+
+        $fsMock->expects($this->once())
+            ->method('dumpFile');
+
+        $file = 'contenido de archivo de prueba';
+        $directorio = 'pruebas';
+        $nombre = 'documento.txt';
+
+        // Sobreescribimos el método internamente para usar el Filesystem mock
+        $refFs = new \ReflectionClass(Filesystem::class);
+        $prop = $refFs->getProperty('fs');
+        $prop->setAccessible(true);
+        $prop->setValue($archivo, $fsMock);
+
+        // Ejecutar manualmente ya que no está inyectado: usamos una versión dummy de saveFile
+        $slugify = new \Cocur\Slugify\Slugify();
+        $slugifiedName = $slugify->slugify('documento') . '.txt';
+
+        $archivo->saveFile($directorio, $nombre, $file, true);
+        $this->assertEquals('pruebas' . DIRECTORY_SEPARATOR . $slugifiedName, $archivo->getRuta());
+        $this->assertTrue($archivo->getVisible());
+    }
+
+    public function testUploadReturnsFalseWhenFileIsNull()
+    {
+        $archivo = new Archivo();
+        $archivo->setFile(null);
+        $result = $archivo->upload('carpeta', 'nombre.pdf');
+        $this->assertFalse($result);
+    }
+
+    public function testUploadSucceedsWithValidFile()
+    {
+        $mockFile = $this->createMock(UploadedFile::class);
+        $mockFile->method('getClientOriginalName')->willReturn('miarchivo.pdf');
+        $mockFile->method('getClientMimeType')->willReturn('application/pdf');
+        $mockFile->expects($this->once())
+            ->method('move');
+
+        $archivo = new Archivo();
+        $archivo->setFile($mockFile);
+        $result = $archivo->upload('pruebas', 'informe.pdf', true);
+
+        $this->assertNotEmpty($result);
+        $this->assertTrue($archivo->getVisible());
+        $this->assertEquals('application/pdf', $archivo->getMimeType());
     }
 }
